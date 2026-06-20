dd7d47291dfd365f84fae7204fe9a61279ffca8b
FishDuM <3057433102@qq.com>
2026-06-20 11:19:46 +0800

    用户模块开发

--- src/main/java/hk/ljx/fishaicode/FishAiCodeApplication.java
+++ src/main/java/hk/ljx/fishaicode/FishAiCodeApplication.java
@@ -1,11 +1,13 @@
 package hk.ljx.fishaicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("hk.ljx.fishaicode.mapper")
 public class FishAiCodeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/hk/ljx/fishaicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package hk.ljx.fishaicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import hk.ljx.fishaicode.modal.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author fish
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
