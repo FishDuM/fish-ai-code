db6d390ecce6c1354b1244f829cd22a26108c9f0
会飞的鱼 <3057433102@qq.com>
2026-07-12 22:11:31 +0800

    LangGraph4j实现并发实现收集图片

--- src/main/java/hk/ljx/fishaicode/langgraph4j/state/WorkflowContext.java
+++ src/main/java/hk/ljx/fishaicode/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package hk.ljx.fishaicode.langgraph4j.state;
 
+import hk.ljx.fishaicode.langgraph4j.model.ImageCollectionPlan;
 import hk.ljx.fishaicode.langgraph4j.model.ImageResource;
 import hk.ljx.fishaicode.langgraph4j.model.QualityResult;
 import hk.ljx.fishaicode.modal.enums.CodeGenTypeEnum;
@@ -77,6 +78,18 @@ public class WorkflowContext implements Serializable {
      * 错误信息
      */
     private String errorMessage;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
 
     @Serial
     private static final long serialVersionUID = 1L;
